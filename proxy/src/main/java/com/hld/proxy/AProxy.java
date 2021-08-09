64eda40a29a9460c7fde08e2d48ceb942aa20177
unknown <591929693@qq.com>
2021-08-09 11:50:35 +0800

    maven center

--- proxy/src/main/java/com/hld/proxy/AProxy.java
+++ proxy/src/main/java/com/hld/proxy/AProxy.java
@@ -12,13 +12,15 @@ public class AProxy {
                 + cls.getSimpleName()+"_112233445566778899Proxy";
         try {
             Class<?> proxyClass=Class.forName(proxyClsName);//加载代理类的class
-            T newObj=(T) proxyClass.getDeclaredConstructor().newInstance();//创建代理类对象，必须有默认构造函数
+//            proxyClass.getDeclaredConstructor().newInstance()
+            Object newObj= proxyClass.getDeclaredConstructor().newInstance();//创建代理类对象，必须有默认构造函数
 
             //获取代理类的setAndroidProxy方法
             Method method=proxyClass.getMethod("_112233445566778899setAndroidProxy",ProxyHandler.class);
             //把handler设置到代理对象中，当调用代理对象中的方法时，就会先执行handler中的方法。
             method.invoke(newObj,handler);
-            return newObj;
+
+            return (T) newObj;
         } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | NoSuchMethodException | IllegalAccessException e1) {
             e1.printStackTrace();
         }
